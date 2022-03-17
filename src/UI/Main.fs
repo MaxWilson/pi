c9882a0c6eedb5592f60d2fdb53f0cd4fdaa9e00
Maximilian Wilson <wilson.max@gmail.com>
2022-03-17 07:17:26 -0700

    Create LearnPi app

--- src/UI/Main.fs
+++ src/UI/Main.fs
@@ -1,4 +1,4 @@
-module UI.Main
+﻿module UI.Main
 
 open UI.Components
 open Browser.Dom
@@ -11,88 +11,128 @@ open Domain
 
 importSideEffects "./styles/global.scss"
 
-type State = { size: int * int; maze: Maze; mode: Maze.MouseMode }
+type State = { input: string; showAnswer: bool }
 type Msg =
-    | Smaller | Bigger | Fresh | AldousBroder | HunterKiller | RandomCarve | RandomPermute | SkipToEnd | Transform of (State -> State)
-let fresh ((x,y) as size) = { size = size; maze = Domain.newMaze(x, y, false); mode = Maze.Inactive }
-let init _ = fresh (20, 12)
+    | Reset
+    | Enter
+    | ChangeInput of string
+let fresh = { input = ""; showAnswer = false }
+let init _ = fresh
 let update msg state =
     match msg with
-    | Smaller ->
-        let (x,y) = state.size
-        fresh (x - 1 |> max 1, y - 1 |> max 1)
-    | Bigger ->
-        let (x,y) = state.size
-        fresh (x + 1, y + 1)
-    | Fresh -> fresh state.size
-    | AldousBroder ->
-        let state = fresh state.size
-        { state with maze = state.maze |> doAlgorithm aldousBroder }
-    | HunterKiller ->
-        let state = fresh state.size
-        { state with maze = state.maze |> doAlgorithm hunterKiller }
-    | RandomCarve ->
-        // remove 30% of interior walls
-        { state with maze = state.maze |> Domain.carve 30 |> normalize }
-    | RandomPermute ->
-        // permute 20% of interior connections
-        { state with maze = state.maze |> Domain.permute 20 |> normalize }
-    | Transform f ->
-        f state
-    | _ -> state
+    | Reset -> fresh
+    | Enter -> { state with showAnswer = true }
+    | ChangeInput input -> { state with input = input; showAnswer = false }
 
+let answer = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067"
+type MatchResult =
+    | ExactMatch of string
+    | Mistakes of correctPrefix: string * rest: string
+    | NotEnough of correctPrefix: string * rest: string
+let getMatch (input:string) (sourceOfTruth:string) =
+    let n = min input.Length sourceOfTruth.Length
+    let matchLength =
+        Seq.zip (input.Substring(0, n)) (sourceOfTruth.Substring(0, n))
+        |> Seq.takeWhile (fun (lhs, rhs) -> lhs = rhs)
+        |> Seq.length
+    if matchLength = sourceOfTruth.Length && matchLength = input.Length then
+        // exact match! Only correct inputs, no wrong values, no missing values
+        ExactMatch input
+    elif matchLength < input.Length then
+        Mistakes(input.Substring(0, matchLength), input.Substring(matchLength, input.Length - matchLength))
+    else
+        NotEnough(input, sourceOfTruth.Substring(matchLength, sourceOfTruth.Length - matchLength))
 let view state dispatch =
-    let modeEvent(mode', connection: Connection option) =
-        match mode', connection with
-        | Maze.CarvingSpace, Some(Connection(x', y', _) as c) when c.isValid() ->
-            Transform (fun state -> { state with mode = mode'; maze = state.maze |> map (fun x y state -> if (x,y) = (x',y') then Open else state) |> normalize })
-            |> dispatch
-        | Maze.BuildingWalls, Some(Connection(x', y', _) as c) when c.isValid() ->
-            let m' = state.maze |> map (fun x y state -> if (x,y) = (x',y') then Closed else state) |> normalize
-            Transform (fun state -> { state with mode = mode'; maze = state.maze |> map (fun x y state -> if (x,y) = (x',y') then Closed else state) |> normalize })
-            |> dispatch
-        | Maze.Inactive, _ ->
-            Transform (fun state -> { state with mode = Maze.Inactive })
-            |> dispatch
-        | _ ->
-            // we're not on a connection currently (it's a corner or space), but activate the proper mode
-            Transform (fun state -> { state with mode = mode' })
-            |> dispatch
+    let enter _ =
+        if state.showAnswer = false then dispatch Enter
+        else dispatch Reset
     Html.div [
-        Html.div [
-            prop.children [
-                Html.text "Click to carve out spaces between cells. Right-click to add walls."
+        prop.style [style.padding 20; style.fontSize 30; style.maxWidth (Browser.Dom.window.innerWidth - 60. |> int)]
+        prop.children [
+            Html.div [
+                Html.text "Type pi in the input box and see how many digits you can get correct!"
                 ]
-            prop.style [style.marginBottom 10]
-            ]
-        Maze.render (state.maze, state.mode, modeEvent)
-        Html.button [
-            prop.text "Reset"
-            prop.onClick (fun _ -> dispatch Fresh)
-            ]
-        Html.button [
-            prop.text "Aldous-Broder"
-            prop.onClick (fun _ -> dispatch AldousBroder)
-            ]
-        Html.button [
-            prop.text "Hunt and Kill"
-            prop.onClick (fun _ -> dispatch HunterKiller)
-            ]
-        Html.button [
-            prop.text "Carve"
-            prop.onClick (fun _ -> dispatch RandomCarve)
-            ]
-        Html.button [
-            prop.text "Permute"
-            prop.onClick (fun _ -> dispatch RandomPermute)
-            ]
-        Html.button [
-            prop.text "Smaller"
-            prop.onClick (fun _ -> dispatch Smaller)
-            ]
-        Html.button [
-            prop.text "Bigger"
-            prop.onClick (fun _ -> dispatch Bigger)
+            Html.div [
+                prop.children [
+                    Html.span [
+                        Html.text "π = "
+                        ]
+                    Html.input [
+                        prop.autoFocus true
+                        prop.style [style.fontSize 30]
+                        prop.size 60
+                        prop.value state.input
+                        prop.onChange (ChangeInput >> dispatch)
+                        prop.onKeyDown (fun e -> if e.code = "Enter" then
+                                                        enter())
+                        ]
+                    Html.button [
+                        prop.style [style.fontSize 30]
+                        prop.text (if state.showAnswer then "RESET" else "Enter")
+                        prop.onClick (fun e -> enter())
+                        ]
+                    ]
+                ]
+            Html.h1 [
+                if state.showAnswer then
+                    match getMatch state.input answer with
+                    | ExactMatch input ->
+                        let decimals = input.Length - 2 |> max 0
+                        Html.text $"Good job! You know pi out to {decimals} decimal places!"
+                        Html.div [
+                            Html.span [
+                                prop.style [style.color color.black]
+                                prop.text input
+                                ]
+                            ]
+                    | Mistakes(input, errors) ->
+                        let decimals = input.Length - 2 |> max 0
+                        Html.div [
+                            prop.style [style.display.block]
+                            prop.children [
+                                Html.span [
+                                    prop.style [style.color color.black]
+                                    prop.text input
+                                    ]
+                                Html.span [
+                                    prop.style [style.color color.red]
+                                    prop.text (answer.Substring(input.Length, min 7 (max (answer.Length - input.Length) 0)))
+                                    ]
+                                ]
+                            ]
+                    | NotEnough(input, rest) ->
+                        let decimals = input.Length - 2 |> max 0
+                        if decimals > 1 then
+                            Html.text $"You know pi out to {decimals} decimal places!"
+                        Html.div [
+                            Html.span [
+                                prop.style [style.color color.black]
+                                prop.text input
+                                ]
+                            Html.span [
+                                prop.style [style.color color.red]
+                                prop.text (rest.Substring(0, min rest.Length 7))
+                                ]
+                            ]
+                else
+                    match getMatch state.input answer with
+                    | ExactMatch input | NotEnough(input, _) ->
+                        Html.span [
+                            prop.style [style.color color.black]
+                            prop.text input
+                            ]
+                    | Mistakes(input, rest) ->
+                        Html.div [
+                            Html.span [
+                                prop.style [style.color color.black]
+                                prop.text input
+                                ]
+                            Html.span [
+                                prop.style [style.color color.red]
+                                prop.text rest
+                                ]
+                            ]
+                ]
             ]
         ]
 
